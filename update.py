c4e9ce165eeb0957ab87b02858e21a27149097ba
lee-mac <lee_excited@usddeiMac.local>
2018-09-15 22:00:59 +0800

    pic2char

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 import pip
 from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
-from time import sleep
 
 for dist in get_installed_distributions():
     call("pip install --upgrade " + dist.project_name, shell=True)
