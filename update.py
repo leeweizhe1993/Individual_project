b1463f96b7014f515bf5cf0a8b3d7d8e58c539ba
lee-mac <lee_excited@usddeiMac.local>
2018-09-18 14:29:44 +0800

    new fix

--- update.py
+++ update.py
@@ -3,4 +3,4 @@ from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
 
 for dist in get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade " + dist.project_name, shell=True)
