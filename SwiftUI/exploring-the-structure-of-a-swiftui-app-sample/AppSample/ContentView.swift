ec4847f69c910dce92c29234a855a39a67ba5831
Weizhe Li <785804871@qq.com>
2024-06-29 14:06:17 +0800

    Swift

---
+++ SwiftUI/exploring-the-structure-of-a-swiftui-app-sample/AppSample/ContentView.swift
@@ -0,0 +1,17 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
